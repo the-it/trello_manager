82563a0f8f67df3261590660bf6b070e5c3dbd2c
Erik Sommer <erotech@posteo.de>
2019-02-17 22:52:40 +0100

    make it more generic

--- terraform/trello_manager.tf
+++ terraform/trello_manager.tf
@@ -1,8 +1,8 @@
-resource "aws_iam_user" "trello_manager" {
+resource "aws_iam_user" "iam_user" {
   name = "trello-manager"
 }
 
-data "aws_iam_policy_document" "assume_lambda" {
+data "aws_iam_policy_document" "lambda_policy" {
   statement {
     sid = "AssumeLambda"
     effect = "Allow"
@@ -32,22 +32,22 @@ data "aws_iam_policy_document" "allow_lambda_exec_document" {
   }
 }
 
-resource "aws_iam_role" "trello_lambda_role" {
+resource "aws_iam_role" "lambda_role" {
   name = "trello-manager-lambda-role"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_lambda.json}"
+  assume_role_policy = "${data.aws_iam_policy_document.lambda_policy.json}"
 }
 
 resource "aws_iam_role_policy" "lambda_exec" {
   name = "trello-manager-lambda-exec"
   policy = "${data.aws_iam_policy_document.allow_lambda_exec_document.json}"
-  role = "${aws_iam_role.trello_lambda_role.id}"
+  role = "${aws_iam_role.lambda_role.id}"
 }
 
-resource "aws_lambda_function" "trello_manager_lambda" {
+resource "aws_lambda_function" "lambda_function" {
   filename = "zip/lambda-${var.lambda_version}.zip"
   description = "Moving some Tasks from A to B."
   function_name = "trello-manager"
-  role = "${aws_iam_role.trello_lambda_role.arn}"
+  role = "${aws_iam_role.lambda_role.arn}"
   handler = "trello_manager.lambda_handler"
   runtime = "python3.7"
   timeout = 30
@@ -55,25 +55,25 @@ resource "aws_lambda_function" "trello_manager_lambda" {
 
 resource "aws_cloudwatch_event_rule" "scheduled_rule" {
   name = "trigger-trello-manager"
-  schedule_expression = "cron(0 0 * * ? *)"
+  schedule_expression = "cron(45 23 * * ? *)"
   is_enabled = true
 }
 
 resource "aws_lambda_permission" "lambda_permission" {
   action = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.trello_manager_lambda.function_name}"
+  function_name = "${aws_lambda_function.lambda_function.function_name}"
   principal = "events.amazonaws.com"
   statement_id = "AllowExecutionForTrigger"
   source_arn = "${aws_cloudwatch_event_rule.scheduled_rule.arn}"
 }
 
 resource "aws_cloudwatch_event_target" "scheduled_rule_target" {
-  arn = "${aws_lambda_function.trello_manager_lambda.arn}"
+  arn = "${aws_lambda_function.lambda_function.arn}"
   rule = "${aws_cloudwatch_event_rule.scheduled_rule.name}"
 }
 
 resource "aws_cloudwatch_log_group" "log_group" {
-  name = "/aws/lambda/${aws_lambda_function.trello_manager_lambda.function_name}"
+  name = "/aws/lambda/${aws_lambda_function.lambda_function.function_name}"
   retention_in_days = 3
 }
 
