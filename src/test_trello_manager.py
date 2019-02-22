e13272bb50fb364d04e6e7fcea1931bb69348a3f
Erik Sommer <erotech@posteo.de>
2019-02-22 19:29:42 +0100

    implementing dirty

--- src/test_trello_manager.py
+++ src/test_trello_manager.py
@@ -4,7 +4,10 @@ from unittest import TestCase
 from testfixtures import compare
 from trello import TrelloClient
 
-from src.trello_manager import TrelloManager, TrelloExecption
+from src.trello_manager import TrelloManager, TrelloExecption, ShoppingTask
+
+
+TEST_BOARD = "UNITTEST"
 
 
 class TestTrelloManager(TestCase):
@@ -16,12 +19,11 @@ class TestTrelloManager(TestCase):
         )
         cls.board = None
         for board in cls.client.list_boards():
-            if "UNITTEST" == board.name:
+            if TEST_BOARD == board.name:
                 cls.board = board
 
     def setUp(self):
-        self.manager = TrelloManager("UNITTEST")
-
+        self.manager = TrelloManager(TEST_BOARD)
 
     def tearDown(self):
         for trello_list in self.board.get_lists(None):
@@ -37,4 +39,33 @@ class TestTrelloManager(TestCase):
         fetched_list = self.manager.get_list_by_name("neue_liste")
         compare(list_id, fetched_list.id)
         compare(None, self.manager.get_list_by_name("bla"))
+    
+    
+ShoppingTask._board = TEST_BOARD
+    
+class TestShoppingTask(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.manager = TrelloManager(TEST_BOARD)
+        cls.client = cls.manager.client
+        cls.board = cls.manager.board
+        cls.task = ShoppingTask()
+
+    def setUp(self):
+        self.board.add_list("Wichtiges Einkaufen")
+        self.buy_list = self.manager.get_list_by_name("Wichtiges Einkaufen")
+        self.label_lebensmittel = self.board.add_label("Lebensmittel", "orange")
+
+    def tearDown(self):
+        for trello_list in self.board.get_lists(None):
+            trello_list.close()
+        
+    def testFetchFromArchive(self):
+        self.buy_list.add_card("Test_Item", labels=[self.label_lebensmittel])
+        self.buy_list.add_card("Don't show")
+        self.buy_list.archive_all_cards()
+
+        compare(1, len(self.task._get_archived_cards()))
+
+
 
--- src/trello_manager.py
+++ src/trello_manager.py
@@ -1,8 +1,7 @@
 import os
-from typing import Union
+import typing
 
-from trello import TrelloClient, Board, List
-
+from trello import TrelloClient, Board, List, Card
 
 
 class TrelloExecption(Exception):
@@ -19,19 +18,60 @@ class TrelloManager:
         if not self.board:
             raise TrelloExecption("Board {} doesn't exists.".format(board))
 
-    def _init_board(self, board_name: str) -> Union[Board, None]:
+    def _init_board(self, board_name: str) -> typing.Union[Board, None]:
         for board in self.client.list_boards():
             if board_name == board.name:
                 return board
         return None
 
-    def get_list_by_name(self, name: str) -> Union[List, None]:
+    def get_list_by_name(self, name: str) -> typing.Union[List, None]:
         for trello_list in self.board.get_lists(None):
             if name == trello_list.name:
                 return trello_list
         return None
 
-if __name__ == "__main__":
-    pass
+class ShoppingTask:
+    _board = "Einkaufen"
 
+    label = {"Drogerie": "Drogerie",
+             "Lebensmittel": "Lebensmittel",
+             "Getränke": "Lebensmittel",
+             "Sonstiges": "Sonstiges"}
+
+    def __init__(self):
+        self.manager = TrelloManager(self._board)
+        self.lists = self._get_lists()
+
+    def _get_archived_cards(self):
+        cards = {}
+        for key in self.label.values():
+            cards[key] = []
+        for card in self.manager.board.get_cards(filters={"filter": "closed"}):
+            if card.labels:
+                for label in card.labels:
+                    if label.name in self.label.keys():
+                        cards[self.label[label.name]].append(card)
+                        break
+        return cards
+
+    def _get_lists(self):
+        lists = {}
+        for list_name in self.label.values():
+            lists[list_name] = self.manager.get_list_by_name(f"Gerade nicht kaufen ({list_name})")
+        return lists
+
+    def _move_to_category(self, card_dict: typing.Dict[str, typing.List[Card]]):
+        for key in card_dict:
+            for card in card_dict[key]:
+                #card.change_list(self.lists[key].id)
+                pass
+
+
+
+
+
+if __name__ == "__main__":
+    task = ShoppingTask()
+    cards = task._get_archived_cards()
+    task._move_to_category(cards)
 
