fb3800cbf81172de5b6bd2865807116559f637ca
Erik Sommer <erotech@posteo.de>
2019-03-02 11:30:43 +0100

    cards are ordered now

--- src/trello_manager.py
+++ src/trello_manager.py
@@ -111,7 +111,7 @@ class ReplayDateTask(TrelloManager):
 
     def run(self):
         self._extract_from_archive()
-        self._sort_replay()
+        self._sort_replay(self.replay_list)
 
     def _extract_from_archive(self):
         print("Processing closed Cards")
@@ -128,8 +128,16 @@ class ReplayDateTask(TrelloManager):
                     continue
                 card.set_due(self.today + timedelta(days=replay_time))
 
-    def _sort_replay(self):
-        pass
+    @staticmethod
+    def _sort_replay(list_to_sort: List):
+        print(f"Sorting Cards on board {list_to_sort}")
+        cards_with_due = []
+        for card in list_to_sort.list_cards():
+            if card.due_date:
+                cards_with_due.append(card)
+        sorted_cards = sorted(cards_with_due, key=lambda list_card: list_card.due)
+        for card in sorted_cards:
+            card.set_pos(1)
 
 
 if __name__ == "__main__":  # pragma: no cover
