ea5eee081f48448a8d781bf06d8d1f307dae1446
Erik Sommer <erotech@posteo.de>
2019-03-14 21:41:21 +0100

    first draft for real cd

---
+++ plan.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+LAMBDA_NAME=trello_manager
+SCRIPT_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
+
+pushd "terraform" > /dev/null
+VERSION=$(cat zip/version.txt)
+
+rm -rf .terraform
+terraform init
+terraform plan -var "lambda_version=${VERSION}" \
+               -var "trello_key=${TRELLO_API_KEY}" \
+               -var "trello_secret=${TRELLO_API_SECRET}"
+
+popd > /dev/null
