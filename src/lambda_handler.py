503835192743dea5973b2643e658c87db84cfaf1
Erik Sommer <ersotech@posteo.de>
2020-12-28 14:52:29 +0100

    add version printing

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -6,6 +6,11 @@ def lambda_handler(event, _):  # pylint: disable=unused-argument
     handler for the lambda framework in the AWS.
     The event is not picked up because lambda is cron triggered.
     """
+    try:
+        with open("version.txt") as version_file:
+            print(f"Current running version is: {version_file.read()}")
+    except FileNotFoundError:
+        print("Local Development Mode")
     print("Move the todo cards on the board")
     ReplayDateTask().run()
     print("Get the Shopping Cards from the archive and sort them")
