fa502c91481fae8b711b70d782cc50b82ae48827
Erik Sommer <ersotech@posteo.de>
2024-02-27 00:27:12 +0100

    update everything to 3.12

--- terraform/modules/trello_manager/lambda.tf
+++ terraform/modules/trello_manager/lambda.tf
@@ -56,7 +56,7 @@ resource "aws_lambda_function" "lambda_function" {
     function_name = "trello-manager"
     role = aws_iam_role.lambda_role.arn
     handler = "lambda_handler.lambda_handler"
-    runtime = "python3.11"
+    runtime = "python3.12"
     timeout = 240
     reserved_concurrent_executions = 1
     environment {
