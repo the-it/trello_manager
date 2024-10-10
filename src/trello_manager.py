bb2ff327b2cba89d9868fa7e7f408058977c0888
Erik Sommer <ersotech@posteo.de>
2024-10-10 10:53:06 +0200

    remove request patch

--- src/trello_manager.py
+++ src/trello_manager.py
@@ -13,60 +13,6 @@ class TrelloExecption(Exception):
     pass
 
 
-def patched_fetch_json(self,
-                       uri_path,
-                       http_method='GET',
-                       headers=None,
-                       query_params=None,
-                       post_args=None,
-                       files=None):
-    """ Fetch some JSON from Trello """
-
-    # explicit values here to avoid mutable default values
-    if headers is None:
-        headers = {}
-    if query_params is None:
-        query_params = {}
-    if post_args is None:
-        post_args = {}
-
-    # if files specified, we don't want any data
-    data = None
-    if files is None and post_args != {}:
-        data = json.dumps(post_args)
-
-    # set content type and accept headers to handle JSON
-    if http_method in ("POST", "PUT", "DELETE") and not files:
-        headers['Content-Type'] = 'application/json; charset=utf-8'
-
-    headers['Accept'] = 'application/json'
-
-    # construct the full URL without query parameters
-    if uri_path[0] == '/':
-        uri_path = uri_path[1:]
-    url = f'https://api.trello.com/1/{uri_path}'
-
-    if self.oauth is None:
-        query_params['key'] = self.api_key
-        query_params['token'] = self.api_secret
-
-    # perform the HTTP requests, if possible uses OAuth authentication
-    response = self.http_service.request(http_method, url, params=query_params,
-                                         headers=headers, data=data,
-                                         auth=self.oauth, files=files,
-                                         proxies=self.proxies)
-
-    if response.status_code == 401:
-        raise trello.Unauthorized(f"{response.text} at {url}", response)
-    if response.status_code != 200:
-        raise trello.ResourceUnavailable(f"{response.text} at {url}", response)
-
-    return response.json()
-
-
-trello.TrelloClient.fetch_json = patched_fetch_json
-
-
 class TrelloManager:  # pylint: disable=too-few-public-methods
     _board_name = None  # type: str
     _key = "TRELLO_API_KEY"
